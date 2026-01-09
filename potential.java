05171086c9592614e4b31ac447595df356cd7816
Bhumika Singh Rawat <bhumika282007@gmail.com>
2026-01-09 18:14:08 +0530

    Potential of string
    Java program to check potential of string.

---
+++ potential.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class potential
+{
+    public static void main()
+    {Scanner sc=new Scanner(System.in);
+        System.out.println(12/0);}}
+       /* String str=sc.next();
+        int len=str.length();
+        char ch=' ';
+        String ex=" ";
+        int ascii,sum=0;
+        String arr[]=new string[len];
+        for(int i=0;i<len;i++)
+        {ch=str.charAt(0);
+            if(ch!=' ');
+            {ex+=ch;}
+            else
+            {j=i;}
+            arr[i]+=ex;
+            System.out.println(arr[i]);}}}
+        */
